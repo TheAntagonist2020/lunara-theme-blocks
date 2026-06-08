0c9abd3f985fe79705414254ded0c27e169f9e1e
RAYLAN <TemporalPincerMove@pm.me>
2026-06-08 17:31:09 -0500

    Update theme anchor for Oscars poster import

--- inc/control-desk.php
+++ inc/control-desk.php
@@ -497,7 +497,7 @@ function lunara_control_desk_get_source_control_status() {
     return array(
         array(
             'label' => __( 'Theme repo', 'lunara-film' ),
-            'value' => 'main @ 67e91c5',
+            'value' => 'main @ 38922c9',
             'state' => 'ready',
             'note'  => 'github.com/TheAntagonist2020/lunara-theme-blocks',
         ),
