4c73eec058b8a58898804e28fb9a03cf81cb8f36
RAYLAN <TemporalPincerMove@pm.me>
2026-06-08 19:11:13 -0500

    Update theme anchor for Review share strip

--- inc/control-desk.php
+++ inc/control-desk.php
@@ -497,7 +497,7 @@ function lunara_control_desk_get_source_control_status() {
     return array(
         array(
             'label' => __( 'Theme repo', 'lunara-film' ),
-            'value' => 'main @ 15951ed',
+            'value' => 'main @ 7020a86',
             'state' => 'ready',
             'note'  => 'github.com/TheAntagonist2020/lunara-theme-blocks',
         ),
