5855ce71318e297682c55e106ce133213a153f8f
RAYLAN <TemporalPincerMove@pm.me>
2026-06-18 16:30:55 -0500

    Update theme anchor for image quality filters

--- inc/control-desk.php
+++ inc/control-desk.php
@@ -751,7 +751,7 @@ function lunara_control_desk_get_source_control_status() {
     return array(
         array(
             'label' => __( 'Theme repo', 'lunara-film' ),
-            'value' => 'main @ 0065db4',
+            'value' => 'main @ c5ad257',
             'state' => 'ready',
             'note'  => 'github.com/TheAntagonist2020/lunara-theme-blocks',
         ),
