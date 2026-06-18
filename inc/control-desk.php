162d2a0822ddb4d2cfb4d2b3dbdcf101f744cbf2
RAYLAN <TemporalPincerMove@pm.me>
2026-06-18 14:30:32 -0500

    Update theme anchor for image console

--- inc/control-desk.php
+++ inc/control-desk.php
@@ -659,7 +659,7 @@ function lunara_control_desk_get_source_control_status() {
     return array(
         array(
             'label' => __( 'Theme repo', 'lunara-film' ),
-            'value' => 'main @ 64c5406',
+            'value' => 'main @ 223dbb6',
             'state' => 'ready',
             'note'  => 'github.com/TheAntagonist2020/lunara-theme-blocks',
         ),
