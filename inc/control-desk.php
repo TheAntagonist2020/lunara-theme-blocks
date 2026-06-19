49802c1abc3a61746b0c2bf9ea495d367eb03e9e
RAYLAN <TemporalPincerMove@pm.me>
2026-06-19 18:22:39 -0500

    Update theme anchor for home Journal sizing

--- inc/control-desk.php
+++ inc/control-desk.php
@@ -839,7 +839,7 @@ function lunara_control_desk_get_source_control_status() {
     return array(
         array(
             'label' => __( 'Theme repo', 'lunara-film' ),
-            'value' => 'main @ 5951ced',
+            'value' => 'main @ bdc7446',
             'state' => 'ready',
             'note'  => 'github.com/TheAntagonist2020/lunara-theme-blocks',
         ),
