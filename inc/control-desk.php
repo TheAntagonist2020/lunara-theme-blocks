00e7898f3350e9988a7fc4a78b60961c91653cf4
RAYLAN <TemporalPincerMove@pm.me>
2026-06-19 12:51:22 -0500

    Update theme anchor for Oscar Fact priority

--- inc/control-desk.php
+++ inc/control-desk.php
@@ -770,7 +770,7 @@ function lunara_control_desk_get_source_control_status() {
     return array(
         array(
             'label' => __( 'Theme repo', 'lunara-film' ),
-            'value' => 'main @ 8be2f1e',
+            'value' => 'main @ 1c911d5',
             'state' => 'ready',
             'note'  => 'github.com/TheAntagonist2020/lunara-theme-blocks',
         ),
