73794df1ab8e33e95d2c79065522c4f0845a0e96
RAYLAN <TemporalPincerMove@pm.me>
2026-06-22 10:06:33 -0500

    Update theme anchor for Utility Search presets

--- inc/control-desk.php
+++ inc/control-desk.php
@@ -3172,7 +3172,7 @@ function lunara_control_desk_get_source_control_status() {
     return array(
         array(
             'label' => __( 'Theme repo', 'lunara-film' ),
-            'value' => 'main @ f00b63e',
+            'value' => 'main @ 6648e1f',
             'state' => 'ready',
             'note'  => 'github.com/TheAntagonist2020/lunara-theme-blocks',
         ),
